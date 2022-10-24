a883373f1bca0651b8940ae654486cb8a040e827
Nilhan Soysa <nilhan3@gmail.com>
2022-10-24 08:53:47 +0530

    nobull2

--- SL/js/main.js
+++ SL/js/main.js
@@ -94,3 +94,14 @@
     
 })(jQuery);
 
+function displayCallTextFunction() {
+    var x = document.getElementById("mycall");
+    var y = document.getElementById("call2registerbtn");
+  //   if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+  //   } else {
+  //     x.style.display = "none";
+  //   }
+  }
+  
--- SL/js/template.js
+++ SL/js/template.js
@@ -540,33 +540,73 @@ class MyAppointmentSpecial extends HTMLElement {
 
 //<!--/////////////////////////guidance end-->///
 
+class CallUsRegister extends HTMLElement {
+    connectedCallback(){
+    this.innerHTML = `
+
+        <div id="mycall">
+            <p class="boldcall">Call / Message to Register Now</p>
+            <p>Please include your name, contact number, whatsapp number, and email in your message. </p>
+
+            <ul class="nobull">
+                <li><a href = "tel:+94777428427">
+                    <i class="bi bi-telephone-fill text-info_sp mx-2" aria-hidden="true">
+                        <!-- ::before -->
+                    </i>
+                    &nbsp;&nbsp;(+94) 0742833337</a>
+
+                </li>
+            
+                <li>
+                    <a href="https://wa.me/+94742833337" target="_blank">
+                        <i class="bi-chat text-info_sp mx-2"aria-hidden="true">
+                        </i>
+                        &nbsp;&nbsp;(+94) 0742833337</a>
+                    
+                </li>
+                                    
+
+            </ul>
+
+        </div>
+
+   
+    `
+    
+        }
+    }
+    customElements.define('my-callr',CallUsRegister)
+
+////////////////////////////////
 class CallUs extends HTMLElement {
     connectedCallback(){
     this.innerHTML = `
-    <p class="boldcall">Call / Message to Register Now</p>
-    <p>Please include your name, contact number, whatsapp number, and email in your message. </p>
-<!-- </button> -->
-<ul class="nobull">
-    <li><a href = "tel:+94777428427">
-        <i class="bi bi-telephone-fill text-info_sp mx-2" aria-hidden="true">
-            <!-- ::before -->
-        </i>
-        &nbsp;&nbsp;(+94) 0742833337</a>
 
-    </li>
-    
-    <li>
-        <a href="https://wa.me/+94742833337" target="_blank">
-            <i class="bi-chat text-info_sp mx-2"aria-hidden="true">
-            </i>
-            &nbsp;&nbsp;(+94) 0742833337</a>
-        
-    </li>
-                        
+        <div id="mycall">
+            <p class="boldcall">Call / Message Us..</p>
+            <p>Please include your name, contact number, whatsapp number, and email in your message. </p>
 
-</ul>
+            <ul class="nobull">
+                <li><a href = "tel:+94777428427">
+                    <i class="bi bi-telephone-fill text-info_sp mx-2" aria-hidden="true">
+                        <!-- ::before -->
+                    </i>
+                    &nbsp;&nbsp;(+94) 0742833337</a>
 
+                </li>
+            
+                <li>
+                    <a href="https://wa.me/+94742833337" target="_blank">
+                        <i class="bi-chat text-info_sp mx-2"aria-hidden="true">
+                        </i>
+                        &nbsp;&nbsp;(+94) 0742833337</a>
+                    
+                </li>
+                                    
 
+            </ul>
+
+        </div>
 
    
     `
@@ -574,7 +614,6 @@ class CallUs extends HTMLElement {
         }
     }
     customElements.define('my-call',CallUs)
-
 
 
 
